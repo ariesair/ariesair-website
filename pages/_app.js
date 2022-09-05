2f07d287f1eb2f8d6255682ea3616e2b39b2004c
chrisachinga <chris.achinga@student.moringaschool.com>
2022-09-05 15:05:22 +0300

    template.ing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+import Script from 'next/head'
+import Layout from '../layout/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Script src='./../scripts/custom.js' async></Script>
+    </Layout>
+  )
 }
 
 export default MyApp
